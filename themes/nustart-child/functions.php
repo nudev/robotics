c22979319948fdec3256a19dc4f4ff01d3b70b2e
tgaffneynu <30910416+nudev@users.noreply.github.com>
2021-10-04 09:15:17 -0400

    Added some missing plugins
    disabled ssl verification within site

--- themes/nustart-child/functions.php
+++ themes/nustart-child/functions.php
@@ -6,7 +6,7 @@
  *
  * @package nustart-child
  */
-// 
+//
 
 
 
@@ -21,13 +21,13 @@ add_action( 'enqueue_block_editor_assets', 'nu__enqueue_block_editor_scripts' );
  * Enqueue frontend and editor JavaScript and CSS
  */
 function nu__enqueue_block_scripts() {
-	
+
     // Enqueue block editor styles
     wp_enqueue_style(
         'nustart-child-main-styles',
 		get_stylesheet_directory_uri() . '/build/style-index.css',
         [],
-        filemtime( get_stylesheet_directory() . '/build/style-index.css' )	
+        filemtime( get_stylesheet_directory() . '/build/style-index.css' )
     );
 
 }
@@ -37,13 +37,13 @@ function nu__enqueue_block_scripts() {
  * Enqueue block JavaScript and CSS for the editor
  */
 function nu__enqueue_block_editor_scripts() {
-	
+
     // Enqueue block editor JS
     wp_enqueue_script(
         'nustart-child-block-editor-scripts',
         get_stylesheet_directory_uri() . '/build/index.js',
         [ 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-components', 'wp-editor' ],
-        filemtime( get_stylesheet_directory() . '/build/index.js' )	
+        filemtime( get_stylesheet_directory() . '/build/index.js' )
     );
 
     // Enqueue block editor styles
@@ -51,10 +51,12 @@ function nu__enqueue_block_editor_scripts() {
         'nustart-child-block-editor-styles',
         get_stylesheet_directory_uri() . '/build/index.css',
         [ 'wp-edit-blocks' ],
-        filemtime( get_stylesheet_directory() . '/build/index.css' )	
+        filemtime( get_stylesheet_directory() . '/build/index.css' )
     );
 
 }
 
+//Disable SSL verification within your testing site. You can do this by adding this line into the file 
+add_filter('https_ssl_verify', '__return_false');
 
 ?>
